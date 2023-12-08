d27011e7d5352e12b5ddc47b603b242605413eb2
vunghiak3 <vunghiak3@gmail.com>
2023-12-08 12:43:05 +0700

    committest

--- app/src/main/java/com/example/mangareader/Fragment_Home.java
+++ app/src/main/java/com/example/mangareader/Fragment_Home.java
@@ -40,7 +40,7 @@ import java.util.ArrayList;
  * Use the {@link Fragment_Home#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment_Home extends Fragment {
+public class    Fragment_Home extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
